f176476e5ca2c8f46485d67f48cec39f69d3c560
Ign0r3dH4x0r <xowmik@gmail.com>
2021-11-24 10:41:01 +0600

    first commit

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+// Back to top
+window.addEventListener("scroll", () => {
+  if (window.scrollY < 300) {
+    document.getElementById("back-to-top").classList.remove("active");
+  } else {
+    document.getElementById("back-to-top").classList.add("active");
+  }
+
+  document.getElementById("copyright").innerText = "Sowmik"
+  document.getElementById("copyright").href = "https://fb.me/ffsowmik"
+
+});
